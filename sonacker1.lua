2f29ecf566869ccdc0bf9e478a37270769ad2db7
hoki-ai <thaothivananh891@gmail.com>
2025-04-27 17:25:04 +0700

    Update sonacker1.lua

--- sonacker1.lua
+++ sonacker1.lua
@@ -37,6 +37,7 @@ local Tabs = {
         Main2=Window:AddTab({ Title="Farm Fruit" }),
         Main3=Window:AddTab({ Title="Farm Chest" }),
         Main4=Window:AddTab({ Title="Script Hop" }),
+	Main5=Window:AddTab({ Title="Auto bounty" }),
 }
     
     Tabs.Main0:AddButton({
@@ -69,81 +70,161 @@ Tabs.Main1:AddButton({
 })
 
 Tabs.Main1:AddButton({
-    Title="Hub",
+    Title="Banana free Hub",
     Description="",
     Callback=function()
-	  
+	  loadstring(game:HttpGet("https://raw.githubusercontent.com/kimprobloxdz/Banana-Free/refs/heads/main/Protected_5609200582002947.lua.txt"))()
   end
 })
 
 Tabs.Main1:AddButton({
-    Title="Hub",
+    Title="Astral Hub",
     Description="",
     Callback=function()
-	  
+	  loadstring(game:HttpGet("https://raw.githubusercontent.com/Overgustx2/Main/refs/heads/main/BloxFruits_25.html"))()
   end
 })
 
 Tabs.Main1:AddButton({
-    Title="Hub",
+    Title="Hiru Hub",
     Description="",
     Callback=function()
-	  
+	  loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaStupid/Source/main/HiruHubVeriV2.lua"))()
   end
 })
 
 Tabs.Main1:AddButton({
-    Title="Hub",
+    Title="Maru free Hub",
     Description="",
     Callback=function()
-	  
+	  loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaCrack/KimP/refs/heads/main/MaruHub"))()
   end
 })
 
 Tabs.Main1:AddButton({
-    Title="Hub",
+    Title="MIN Doreamon Hub",
     Description="",
     Callback=function()
-	  
+	   loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaCrack/Min/refs/heads/main/MinMV"))()
   end
 })
 
 Tabs.Main1:AddButton({
-    Title="Hub",
+    Title="Tsuo Hub",
     Description="",
     Callback=function()
-	  
+	  loadstring(game:HttpGet("https://raw.githubusercontent.com/Tsuo7/TsuoHub/main/Tsuoscripts"))()
+  end
+})
+
+Tabs.Main5:AddButton({
+    Title="Xero bounty Hub",
+    Description="",
+    Callback=function()
+	  loadstring(game:HttpGet("https://raw.githubusercontent.com/pisovn/OisiRoblox1/refs/heads/main/AutoBountyXero"))()
   end
 })
 
 Tabs.Main1:AddButton({
-    Title="Hub",
+    Title="Monster Hub",
     Description="",
     Callback=function()
-	  
+	  loadstring(game:HttpGet("https://raw.githubusercontent.com/giahuy2511-coder/MonsterHub/refs/heads/main/MonsterHub"))() 
   end
 })
 
 Tabs.Main1:AddButton({
-    Title="Hub",
+    Title="Xero Hub",
     Description="",
     Callback=function()
-	  
+	  loadstring(game:HttpGet("https://raw.githubusercontent.com/Xero2409/XeroHub/refs/heads/main/main.lua"))()
+  end
+})
+
+Tabs.Main3:AddButton({
+    Title="Nhặt rương",
+    Description="",
+    Callback=function()
+	  loadstring(game:HttpGet("https://raw.githubusercontent.com/xxhumggxx/SkullHub/refs/heads/main/ChestV2.lua"))()
   end
 })
 
 Tabs.Main1:AddButton({
-    Title="Hub",
+    Title="Rubu v3Hub",
     Description="",
     Callback=function()
-	  
+	  loadstring(game:HttpGet("https://raw.githubusercontent.com/Bubu2k/Rubutv/refs/heads/main/RubuHubV3.txt"))()
   end
 })
 
 Tabs.Main1:AddButton({
-    Title="Hub",
+    Title="Rubu v4 Hub",
     Description="",
     Callback=function()
-	  
+	  loadstring(game:HttpGet("https://raw.githubusercontent.com/Bubu2k/Rubutv/refs/heads/main/RuBuV4.lua"))()
+  end
+})
+
+Tabs.Main1:AddButton({
+    Title="W azure Hub",
+    Description="",
+    Callback=function()
+	  loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/3b2169cf53bc6104dabe8e19562e5cc2.lua"))()
+  end
+})
+
+Tabs.Main1:AddButton({
+    Title="Rubu v2 Hub",
+    Description="",
+    Callback=function()
+	  loadstring(game:HttpGet("https://raw.githubusercontent.com/Bubu2k/Rubutv/refs/heads/main/rubuhubv2"))()
+  end
+})
+
+Tabs.Main4:AddButton({
+    Title="Đổi sever Hub",
+    Description="",
+    Callback=function()
+	  loadstring(game:HttpGet("https://raw.githubusercontent.com/tuyenkimp/sever/refs/heads/main/kimpsever"))()
+  end
+})
+
+Tabs.Main1:AddButton({
+    Title="Trẩu roblox Hub",
+    Description="",
+    Callback=function()
+	  loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaCrack/TrauHub/refs/heads/main/TrauTX"))()
+  end
+})
+
+Tabs.Main2:AddButton({
+    Title="Maris Hub",
+    Description="",
+    Callback=function()
+	  loadstring(game:HttpGet("https://raw.githubusercontent.com/marisdeptrai/Script-Free/main/FruitFinder.lua"))()
+  end
+})
+
+Tabs.Main2:AddButton({
+    Title="Turbo Hub",
+    Description="",
+    Callback=function()
+	  loadstring(game:HttpGet("https://raw.githubusercontent.com/TurboLite/Script/refs/heads/main/TraiCay.lua"))()
+  end
+})
+
+Tabs.Main1:AddButton({
+    Title="ZIS Doraemon Hub",
+    Description="",
+    Callback=function()
+	  loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaCrack/Zis/refs/heads/main/ZisRb11"))()
+  end
+})
+
+Tabs.Main1:AddButton({
+    Title="Rubo_memem Hub",
+    Description="",
+    Callback=function()
+	  loadstring(game:HttpGet("https://raw.githubusercontent.com/hoki-ai/rubo_meme-boy/refs/heads/main/rubo_memem.lua.txt"))()
   end
 })
